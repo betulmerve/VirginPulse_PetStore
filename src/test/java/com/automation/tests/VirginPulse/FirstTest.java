a791608306e63e23476fe5b9e297b93d79e53acc
betulmerve <merve.faki@gmail.com>
2021-08-31 23:13:06 -0400

    test

--- src/test/java/com/automation/tests/VirginPulse/FirstTest.java
+++ src/test/java/com/automation/tests/VirginPulse/FirstTest.java
@@ -1,12 +1,19 @@
 package com.automation.tests.VirginPulse;
 
+
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+import java.awt.*;
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class FirstTest {
 
@@ -42,45 +49,42 @@ public class FirstTest {
     }
 
 
+
     @Test
-    @DisplayName("Get a specific pet")
-    public void getPet() {
-        Response response= given().baseUri(BASE_URL).
-                when().get("/pet/{petId}",100).prettyPeek();
+    @DisplayName("Create a pet and add a picture")
+    public void createAndAddPicToPet() {
+
+        File imageFile = new File("/Users/mervefaki/IdeaProjects/VirginPulse_PetStore/download.jpeg");
+
+        String body="{\n" +
+                "  \"id\": 0,\n" +
+                "  \"category\": {\n" +
+                "    \"id\": 0,\n" +
+                "    \"name\": \"string\"\n" +
+                "  },\n" +
+                "  \"name\": \"string\",\n" +
+                "  \"photoUrls\": [\n" +
+                "    \"string\"\n" +
+                "  ],\n" +
+                "  \"tags\": [\n" +
+                "    {\n" +
+                "      \"id\": 0,\n" +
+                "      \"name\": \"string\"\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"status\": \"available\"\n" +
+                "}";
+
+
+
+                given().
+                        multiPart("file",new File("/Users/mervefaki/IdeaProjects/VirginPulse_PetStore/download.jpeg")).
+                        body(body).baseUri(BASE_URL).
+                when().
+                        post("/pet/{petId}/uploadImage",200).prettyPeek().
+                        then().statusCode(200);
 
     }
-
-
-//    @Test
-//    @DisplayName("Create a pet and add a picture")
-//    public void createAndAddPicToPet() {
-//
-//        File imageFile = new File(System.getProperty("user.dir") + "/download.jpeg");
-//
-//        String body="{\n" +
-//                "  \"id\": 0,\n" +
-//                "  \"category\": {\n" +
-//                "    \"id\": 0,\n" +
-//                "    \"name\": \"string\"\n" +
-//                "  },\n" +
-//                "  \"name\": \"doggie\",\n" +
-//                "  \"photoUrls\": [\n" + imageFile +
-//                "    \"string\"\n" +
-//                "  ],\n" +
-//                "  \"tags\": [\n" +
-//                "    {\n" +
-//                "      \"id\": 0,\n" +
-//                "      \"name\": \"string\"\n" +
-//                "    }\n" +
-//                "  ],\n" +
-//                "  \"status\": \"available\"\n" +
-//                "}";
-//
-//
-//        Response response= given().contentType(ContentType.JSON).body(body).baseUri(BASE_URL).
-//                when().post("/pet/{petId}/uploadImage",2000).prettyPeek();
-//
-//    }
 
 
 
--- src/test/java/com/automation/tests/VirginPulse/SecondTest.java
+++ src/test/java/com/automation/tests/VirginPulse/SecondTest.java
@@ -1,19 +1,11 @@
 package com.automation.tests.VirginPulse;
 
-
-import com.fasterxml.jackson.core.type.TypeReference;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
 
 public class SecondTest {
 
@@ -27,10 +19,28 @@ public class SecondTest {
                     when().get("/pet/findByStatus").prettyPeek();
 
 
+
         }
 
-        @Test
-        @DisplayName("djsbfk")
+
+    @Test
+    @DisplayName("Get a specific pet")
+    public void getPet() {
+        Response response=given().baseUri(BASE_URL).
+                when().get("/pet/{petId}",200).prettyPeek();
+
+        response.
+                then().assertThat().statusCode(200).
+                and().
+                body("id",is(200));
+
+    }
+
+
+
+
+    }
+
 
 
     //    @Test
@@ -40,53 +50,5 @@ public class SecondTest {
 //                when().get("/pet/findByStatus").prettyPeek();
 //
 //    }
-//try
 
-//    @Test
-//    public void getAllKeysFromNestedJsonObjectUsingMap() throws IOException {
-//
-//        String jsonObject ="{\n" +
-//                "  \"id\": 0,\n" +
-//                "  \"category\": {\n" +
-//                "    \"id\": 0,\n" +
-//                "    \"name\": \"string\"\n" +
-//                "  },\n" +
-//                "  \"name\": \"string\",\n" +
-//                "  \"photoUrls\": [\n" +
-//                "    \"string\"\n" +
-//                "  ],\n" +
-//                "  \"tags\": [\n" +
-//                "    {\n" +
-//                "      \"id\": 0,\n" +
-//                "      \"name\": \"string\"\n" +
-//                "    }\n" +
-//                "  ],\n" +
-//                "  \"status\": \"available\"\n" +
-//                "}";
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        // Deserialize into Map
-//        Map<String, Object> parsedJsonObject = objectMapper.readValue(jsonObject, new TypeReference<Map<String, Object>>() {
-//        });
-//        // Get all keys
-//        Set<String> allKeys = parsedJsonObject.keySet();
-//        // Iterate keys
-//        allKeys.stream().forEach(key -> {
-//            Object value = parsedJsonObject.get(key);
-//            // If value is a String. You may need to add more if value is of different types
-//            if (value instanceof String)
-//                System.out.println(key);
-//                // If value is another JSON Object which will be LinkedHashMap. You can see this while debugging
-//            else if (value instanceof LinkedHashMap<?, ?>) {
-//                @SuppressWarnings("unchecked")
-//                Set<String> allKeysOfNestedJsonObject = ((LinkedHashMap<String, ?>) value).keySet();
-//                allKeysOfNestedJsonObject.stream().forEach(k -> System.out.println(k));
-//            }
-//        });
-//    }
-
-
-
-
-
-    }
 
